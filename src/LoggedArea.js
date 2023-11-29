c3ba768cf3dc7c837fcd49b01606357cc036188c
anderson <anderson@petshopcontrol.com.br>
2023-11-28 22:24:27 -0300

    grid para selecionar animal no agendamento de servico

--- src/LoggedArea.js
+++ src/LoggedArea.js
@@ -1,127 +1,126 @@
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importe o CSS do Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import './LoggedArea.css'; // Você pode criar seu próprio arquivo CSS para personalização
+import './LoggedArea.css';
 import axios from 'axios';
 
 function LoggedArea() {
-  // Dados de exemplo para os serviços
   const services = [
     {
       category: 'Estética',
       name: 'Tosa',
       price: 50,
-      availableTimes: ['09:00 AM', '10:00 AM', '02:00 PM'], // Exemplo de horários disponíveis
+      availableTimes: ['09:00 AM', '10:00 AM', '02:00 PM'],
     },
     {
       category: 'Estética',
       name: 'Banho',
       price: 30,
-      availableTimes: ['11:00 AM', '01:00 PM', '03:00 PM'], // Exemplo de horários disponíveis
+      availableTimes: ['11:00 AM', '01:00 PM', '03:00 PM'],
     },
     // Adicione mais serviços aqui
   ];
 
-  // Estado para controlar qual botão "Ver Horários Disponíveis" foi clicado, qual horário foi selecionado, a data selecionada e se o botão de confirmação deve ser exibido
   const [clickedButton, setClickedButton] = useState(null);
   const [selectedTime, setSelectedTime] = useState(null);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [showConfirmation, setShowConfirmation] = useState(false);
-
-  // Estado para rastrear o botão de hora selecionado
   const [selectedButton, setSelectedButton] = useState(null);
-
-  // Estado para controlar se o card está aberto ou fechado
   const [cardOpen, setCardOpen] = useState(false);
   const [submenuOpen, setSubmenuOpen] = useState(false);
-  // Função para lidar com o clique no botão "Ver Horários Disponíveis"
+  const [selectedAnimal, setSelectedAnimal] = useState(null);
+
   const handleShowTimes = (index) => {
-    // Se o mesmo botão for clicado novamente, feche o card
     if (clickedButton === index && cardOpen) {
       setCardOpen(false);
     } else {
-      // Caso contrário, defina o botão clicado e abra o card
       setClickedButton(index);
       setCardOpen(true);
     }
 
-    // Limpa o horário selecionado quando um novo botão "Ver Horários Disponíveis" é clicado
     setSelectedTime(null);
     setSelectedButton(null);
     setShowConfirmation(false);
   };
-  // Função para lidar com o clique no botão do submenu
+
   const handleSubmenuToggle = () => {
     setSubmenuOpen(!submenuOpen);
   };
 
-  // Função para lidar com o clique em "Meu Cadastro"
   const handleMyProfileClick = () => {
-    // Implemente a navegação para a página de perfil do usuário
     alert('Navegar para a página de perfil do usuário');
   };
-   // Função para lidar com o clique em "Sair"
+
   const handleLogoutClick = () => {
-    // Implemente a lógica de logout aqui
     alert('Usuário deslogado');
   };
-  // Função para lidar com o clique em um horário disponível
+
   const handleTimeClick = (time) => {
     setSelectedTime(time);
-    setShowConfirmation(true); // Mostra o botão de confirmação quando um horário é selecionado
-
-    // Define o botão de hora selecionado
+    setShowConfirmation(true);
     setSelectedButton(time);
   };
 
-  // Função para lidar com o clique no botão de confirmação
   const handleConfirmation = () => {
-const Agenda={
-  AgendaID:0,
-  IDAnimal:2,
-  IDHorario:1,
-  AtualizadoSistema:false,
-  IDEmpresa:1,
-  IDServicoCategoria:1
-}
+    const Agenda = {
+      AgendaID: 0,
+      IDAnimal: selectedAnimal, // Use o animal selecionado
+      IDHorario: 1,
+      AtualizadoSistema: false,
+      IDEmpresa: 1,
+      IDServicoCategoria: 1,
+    };
 
-axios.post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
-.then(response => {
-  // Lógica para lidar com a resposta da API (se necessário)
-  console.log('Resposta da API:', response.data);
-  alert(`Agendamento confirmado para ${selectedTime}`);
-})
-.catch(error => {
-  // Lógica para lidar com erros na solicitação
-  console.error('Erro ao confirmar agendamento:', error);
-  alert('Erro ao confirmar agendamento. Por favor, tente novamente.');
-});
-
- 
+    axios
+      .post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
+      .then((response) => {
+        console.log('Resposta da API:', response.data);
+        alert(`Agendamento confirmado para ${selectedTime}`);
+      })
+      .catch((error) => {
+        console.error('Erro ao confirmar agendamento:', error);
+        alert('Erro ao confirmar agendamento. Por favor, tente novamente.');
+      });
   };
 
-  // Função para lidar com a mudança de data
   const handleDateChange = (date) => {
     setSelectedDate(date);
-    setSelectedTime(null); // Limpa o horário selecionado quando a data muda
-    setSelectedButton(null); // Limpa o botão de hora selecionado quando a data muda
+    setSelectedTime(null);
+    setSelectedButton(null);
     setShowConfirmation(false);
   };
 
-  // Função para renderizar os cards dos serviços
+  const handleAnimalSelect = (animalId) => {
+    setSelectedAnimal(animalId);
+  };
+
   const renderServiceCards = () => {
     return services.map((service, index) => (
-      <div
-        key={index}
-        className={`col-lg-4 col-md-6 mb-4`}
-      >
+      <div key={index} className={`col-lg-4 col-md-6 mb-4`}>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{service.name}</h5>
             <p className="card-text">Categoria: {service.category}</p>
             <p className="card-text">Preço: R${service.price}</p>
+
+            <div className="form-group">
+              <label htmlFor={`animalSelect-${index}`}>Selecionar Animal:</label>
+              <select
+                id={`animalSelect-${index}`}
+                className="form-control"
+                onChange={(e) => handleAnimalSelect(e.target.value)}
+              >
+                <option value="" disabled selected>
+                  Escolha um animal
+                </option>
+                {/* Adicione as opções de animais aqui */}
+                <option value="1">Animal 1</option>
+                <option value="2">Animal 2</option>
+                {/* Adicione mais opções conforme necessário */}
+              </select>
+            </div>
           </div>
-          {/* Exibir o botão "Ver Horários Disponíveis" */}
+
           <div className="card-footer">
             <button
               className="btn btn-primary btn-sm float-right"
@@ -130,7 +129,7 @@ axios.post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
               Ver Horários Disponíveis
             </button>
           </div>
-          {/* Exibir os botões de horários disponíveis apenas se o card estiver aberto */}
+
           {clickedButton === index && cardOpen && (
             <div className="card-footer">
               <div className="input-group mb-3">
@@ -140,7 +139,7 @@ axios.post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
                 <input
                   type="date"
                   className="form-control"
-                  value={selectedDate.toISOString().split('T')[0]} // Formate a data para o formato 'YYYY-MM-DD'
+                  value={selectedDate.toISOString().split('T')[0]}
                   onChange={(e) => handleDateChange(new Date(e.target.value))}
                 />
               </div>
@@ -157,7 +156,7 @@ axios.post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
                   </button>
                 ))}
               </div>
-              {/* Exibir o botão de confirmação apenas se um horário for selecionado */}
+
               {showConfirmation && (
                 <div className="card-footer">
                   <button className="btn btn-primary mt-2" onClick={handleConfirmation}>
@@ -174,54 +173,46 @@ axios.post('https://localhost:44358/api/v1/Agenda/cadagenda', Agenda)
 
   return (
     <div className="LoggedArea">
-    {/* Navbar */}
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          PetShop
-        </a>
-      
-        {/* Centralize a barra de pesquisa */}
-        <div className="mx-auto">
-          <form className="form-inline my-2 my-lg-0">
-            <div className="input-group">
-              <input
-                className="form-control"
-                type="search"
-                placeholder="Pesquisar"
-                aria-label="Pesquisar"
-              />
-              <div className="input-group-append">
-                <button
-                  className="btn btn-outline-success"
-                  type="submit"
-                >
-                  Pesquisar
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <NavDropdown title="Opcões" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Meus Dados</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-               Sair
-              </NavDropdown.Item>
-              {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item> */}
-            </NavDropdown>
-      </div>
-    </nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            PetShop
+          </a>
 
-    {/* Conteúdo principal */}
-    <div className="container my-5">
-      <h1>Serviços Disponíveis</h1>
-      <div className="row">{renderServiceCards()}</div>
+          <div className="mx-auto">
+            <form className="form-inline my-2 my-lg-0">
+              <div className="input-group">
+                <input
+                  className="form-control"
+                  type="search"
+                  placeholder="Pesquisar"
+                  aria-label="Pesquisar"
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-outline-success" type="submit">
+                    Pesquisar
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <NavDropdown title="Opcões" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1" onClick={handleMyProfileClick}>
+              Meus Dados
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2" onClick={handleLogoutClick}>
+              Sair
+            </NavDropdown.Item>
+          </NavDropdown>
+        </div>
+      </nav>
+
+      <div className="container my-5">
+        <h1>Serviços Disponíveis</h1>
+        <div className="row">{renderServiceCards()}</div>
+      </div>
     </div>
-  </div>
   );
 }
 
